41cd7d90ad790c1acb0f6c326f37e38e31020f80
freelancercoder9 <freelancercoder9@gmail.com>
2021-07-02 14:37:07 +0800

    modified sec4

--- src/components/Section4.js
+++ src/components/Section4.js
@@ -10,7 +10,7 @@ function Section4() {
     <div className="">
       <div className="sec1-heading">Extra Savings</div>
       <div className="images-sec4 d-flex">
-        <div className="div-left check">
+        <div className="div-outer">
           <div className="img1">
             <img src={bag} className="w-100" />
           </div>
@@ -18,20 +18,26 @@ function Section4() {
             <img src={woodlandsShoes} className="w-100" />
           </div>
         </div>
-        <div className="div-middle check">
+        <div className="div-middle ">
           <div>
             <img src={humor} className="w-100" />
           </div>
-          <div className="fs-1 fw-bold text-center">Get Now!</div>
-          <div className="div-btn">
-            <button type="button" class="btn-img3 ">
-              Shop Now
-            </button>
+          <div className="middle-text">
+            <div className=" text-middle  fw-bold text-center">Get Now!</div>
+            <div className="div-btn">
+              <button type="button" class="btn-img3 ">
+                Shop Now
+              </button>
+            </div>
           </div>
         </div>
-        <div className="div-right check">
-          <div className="img4"></div>
-          <div className="img5"></div>
+        <div className="div-outer">
+          <div className="img1">
+            <img src={body} className=" w-100" />
+          </div>
+          <div className="img2">
+            <img src={scarf} className=" w-100" />
+          </div>
         </div>
       </div>
     </div>
